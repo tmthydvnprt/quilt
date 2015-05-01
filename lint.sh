7a6a796b90c485efc9241d4ffa32fcc329477c29
Timothy Davenport <davenport.tim@gmail.com>
2015-05-01 05:56:13 -0700

    add timestamp to linting report

--- lint.sh
+++ lint.sh
@@ -4,11 +4,18 @@
 find . -name "*.py" | xargs sed -i '' -e's/[ ^I]*$//'
 
 # lint project
-echo 'quilt' > linting_report.txt
+echo '' > linting_report.txt
+echo 'Quilt Linting Report' >> linting_report.txt
+echo `date` >> linting_report.txt
+echo '=========================================' >> linting_report.txt
+echo '' >> linting_report.txt
 pylint quilt >> linting_report.txt
 
-echo ' ' >> linting_report.txt
-echo 'tests' >> linting_report.txt
+echo '\n\n' >> linting_report.txt
+echo 'Tests Linting Report' >> linting_report.txt
+echo `date` >> linting_report.txt
+echo '=========================================' >> linting_report.txt
+echo '' >> linting_report.txt
 pylint tests >> linting_report.txt
 
 echo "project linted"
